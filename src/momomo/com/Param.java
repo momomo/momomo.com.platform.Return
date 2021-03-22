581f11d4b804faaa5942b2328c163c2df08d76f2
momomo <opensource@momomo.com>
2021-03-22 10:21:03 +0100

    No message

---
+++ src/momomo/com/Param.java
@@ -0,0 +1,10 @@
+package momomo.com;
+
+/**
+ * Maybe overkill? But nice syntactic sugar for method params instead. 
+ * 
+ * @see momomo.com.platform.Return.examples.Examples#paramsThree(Param.Three) for example and usage in the same class. 
+ * 
+ * @author Joseph S.
+ */
+public final class Param extends Return { private Param(){ super(); } }
